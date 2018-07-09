65e353b828053437ff2846f992884db5a3200229
teerawat.th <teerawat.th@baac.or.th>
2018-07-09 14:55:54 +0700

    [CREATED]:สร้าง connect database และ query

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type UserData struct {
+	Id              int
+	CitizenId       string
+	FirstName       string
+	LastName        string
+	BirthYear       int
+	FirstnameFather string
+	LastnameFather  string
+	FirstnameMother string
+	LastnameMother  string
+	SoldierId       int
+	AddressId       int
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		fmt.Println("connect fail")
+	}
+	fmt.Println("connect success")
+
+	defer db.Close()
+
+	results, _ := db.Query("SELECT * FROM user") // result จะ return ออกมาเป็น struc เราต้องทำ struc มารองรับ
+
+	for results.Next() {
+		var userData UserData
+
+		err = results.Scan(
+			&userData.Id,
+			&userData.CitizenId,
+			&userData.FirstName,
+			&userData.LastName,
+			&userData.BirthYear,
+			&userData.FirstnameFather,
+			&userData.LastnameFather,
+			&userData.FirstnameMother,
+			&userData.LastnameMother,
+			&userData.SoldierId,
+			&userData.AddressId,
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(userData)
+	}
+}
